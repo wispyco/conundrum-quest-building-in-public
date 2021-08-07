56e997ca088b93a7c61a04eedb0f35d396696bd2
Anders Mac Mini <hi@wispy.co>
2021-08-07 08:29:41 -0600

    metadata and bottom title change

--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,25 @@
-import Alert from '../components/alert'
-import Links from './links'
-import Meta from '../components/meta'
+import Alert from "../components/alert";
+import Links from "./links";
+import Meta from "../components/meta";
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
       <Links />
-      <h1 className="text-7xl text-center"><a href="https://conundrum.quest">Conundrum Quest</a></h1>
+      <h1 className="text-7xl text-center">
+        <a href="https://conundrum.quest">Conundrum Quest</a>
+      </h1>
       <div className="py-20">
         <main>{children}</main>
       </div>
-      <a className="text-center text-2xl py-10 underline w-1/2 m-auto block" href="https://conundrum.quest">Visit Conundrum Quest - The worlds hardest problems and who is working on them</a>
+      <a
+        className="text-center text-2xl py-10 underline w-1/2 m-auto block"
+        href="https://conundrum.quest"
+      >
+        Visit Conundrum Quest - A place to share new problems, ideas and see who
+        is working on them.
+      </a>
     </>
-  )
+  );
 }
